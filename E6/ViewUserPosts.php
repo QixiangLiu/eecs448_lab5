2a7ed5dfb025be604e42cf3d0b9aba060cfd52af
Qixiang Liu <q709l816@ku.edu>
2018-03-12 15:28:54 -0500

    Fix E3 and finish E6

--- E6/ViewUserPosts.php
+++ E6/ViewUserPosts.php
@@ -1,21 +1,21 @@
 <?php
-echo "123";
-/*
-$username ="";
-$mysqli = new mysqli("mysqli.eecs.ku.edu","q709l816","eegh3eJ7","q709l816");
-if($mysqli->connect_errno){
-  printf("Connection Failed! %s\n",$mysqli->connect_error);
-  exit();
-}
-$query = "SELECT user_id FROM Users";
-if($result->query(query)){
-  while($row->$result->fetch_assoc()){
-    $username = $row["user_id"];
-    echo "<option value='$username'>".$username."</option>";
-    echo "<br>";
+  echo "<html><body><table>";
+  $select = $_POST["select"];
+  $mysqli = new mysqli("mysql.eecs.ku.edu","q709l816","eegh3eJ7","q709l816");
+  if($mysqli->connect_errno){
+    printf("Connection Failed! %s\n",$mysqli->connect_error);
+    exit();
   }
 
-}
+    $query = "SELECT content FROM Posts WHERE author_id = '$select'";
+    if($result=$mysqli->query($query)){
+      while($row=$result->fetch_assoc()){
+          echo "<tr><td>".$row["content"]."</td></tr>";
+      }
+    }else{
+      echo "<tr><td>Nothing to post</td></tr>";
+    }
+    $mysqli->close();
+    echo "</table></body></html>";
 
-*/
 ?>
